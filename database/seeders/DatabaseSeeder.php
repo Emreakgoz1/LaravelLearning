a7ac518a8e0762675c2ffb7110d179bf3a83d74a
Emreakgoz1 <baranemreakgoz@gmail.com>
2023-04-16 12:50:48 +0300

    Laravel Database Management Added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([SayfaSeeder::class]);
+
+
+
+
+
+        //  \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //   'name' => 'Test User',
+        // 'email' => 'test@example.com',
+        //]);
     }
 }
